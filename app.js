a8363f64b4fd853470dbbf6ae59c090605e2ea87
codingxllama <osamaalchalabi2@gmail.com>
2023-07-20 00:00:00 -0400

    UPDATE TIMER

--- app.js
+++ app.js
@@ -1,30 +1,34 @@
-const myDate = new Date();
-let hour = myDate.getHours();
-console.log(hour); // 11pm
+function updateTime() {
+  const myDate = new Date();
+  let hour = myDate.getHours();
+  console.log(hour); // 11pm
 
-let minute = myDate.getMinutes();
-console.log(minute);
+  let minute = myDate.getMinutes();
+  console.log(minute);
 
-let prepend = hour >= 12 ? "PM" : "AM";
-console.log(prepend);
+  let prepend = hour >= 12 ? "PM" : "AM";
+  console.log(prepend);
 
-let myTime = hour + ":" + minute + prepend;
-let user = "Smokey";
+  let myTime = hour + ":" + minute + prepend;
+  let user = "Smokey";
 
-if (hour >= 12 && hour <= 17) {
-  // afternoon = 12pm to 5pm (17)
+  if (hour >= 12 && hour <= 17) {
+    // afternoon = 12pm to 5pm (17)
 
-  console.log(`Good afternoon${user}`);
-  //evening = 5pm to 8pm
-} else if (hour >= 17 && hour <= 20) {
-  //evening = 5pm to 8pm
-  console.log(`Good evening${user}`);
-} else if (hour >= 20 || hour <= 6) {
-  //8pm to 6am
-  console.log(`Good night ${user}`);
-} else {
-  console.log(`Good Morning${user}`);
+    console.log(`Good afternoon${user}`);
+    //evening = 5pm to 8pm
+  } else if (hour >= 17 && hour <= 20) {
+    //evening = 5pm to 8pm
+    console.log(`Good evening${user}`);
+  } else if (hour >= 20 || hour <= 6) {
+    //8pm to 6am
+    console.log(`Good night ${user}`);
+  } else {
+    console.log(`Good Morning${user}`);
+  }
+
+  let currentTime = document.getElementById("currentTime");
+  currentTime.innerHTML = `The current time is ${myTime}`;
 }
 
-let currentTime = document.getElementById("currentTime");
-currentTime.innerHTML = `The current time is ${myTime}`;
+setInterval(updateTime, 1000);
